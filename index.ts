ff97f93b4c7e5a1a2bd943a8b1997854894724de
Peter Knipp <peteraknipp@gmail.com>
2024-11-04 15:08:18 -0500

    Minor changes.

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 
+import double from './helpers/double';
+
 const PORT = process.env.PORT || 5001;
 const server = express();
 
@@ -10,7 +12,7 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/:data', (req, res) => {
-    const data = req.params.data;
+    const data = double(req.params.data);
     res.render('pages/result', {data});
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
